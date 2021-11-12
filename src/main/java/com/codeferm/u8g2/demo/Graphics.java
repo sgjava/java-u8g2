760386fc2fe0316b6e197c38f6531db895b8797a
servadmin <servadmin@ubuntuserv>
2021-11-11 22:36:10 -0500

    Default delay to 0

--- src/main/java/com/codeferm/u8g2/demo/Graphics.java
+++ src/main/java/com/codeferm/u8g2/demo/Graphics.java
@@ -88,7 +88,7 @@ public class Graphics implements Callable<Integer> {
      * Nanosecond delay or 0 for none for software I2C and SPI.
      */
     @Option(names = {"--delay"}, description = "Nanosecond delay for software I2c and SPI (0 default)")
-    private long delay = 14;
+    private long delay = 0;
     /**
      * Pointer to u8g2_t struct.
      */
